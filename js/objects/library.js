5d481f75093f7c8691e4e8d29b682dc677d49c74
Thomas Lecoq <37746451+thomas-lecoq@users.noreply.github.com>
2026-06-01 19:33:52 +0200

    style(library): add missing semicolons

--- js/objects/library.js
+++ js/objects/library.js
@@ -2,10 +2,10 @@ import { Book } from "./book.js"
 
 function Library(maxSize){
     if (!new.target) {
-        throw Error("You must use the 'new' operator to call the constructor")
+        throw Error("You must use the 'new' operator to call the constructor");
     }
     if (maxSize <= 0) {
-        throw Error("A Library must have a non negative size")
+        throw Error("A Library must have a non negative size");
     }
     this.bookList = [];
     this.maxSize = maxSize;
