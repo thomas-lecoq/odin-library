26c2626e54c9d72246593485356485b5f497a73a
Thomas Lecoq <37746451+thomas-lecoq@users.noreply.github.com>
2026-06-01 18:48:40 +0200

    feat(library): add addBookToLibrary function
    The library Object didn't have a function to add Book for to a given
    Library. This function now pushes Book object to library bookList
    property.

--- js/objects/library.js
+++ js/objects/library.js
@@ -4,10 +4,19 @@ function Library(maxSize){
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor")
     }
-
-    if (maxSize <= 0) {
+    if (maxSize <= 0) {
         throw Error("A Library must have a non negative size")
     }
     this.bookList = [];
     this.maxSize = maxSize;
+}
+
+function addBookToLibrary(book, library) {
+    if (!book instanceof Book) {
+        throw Error("The element passed for 'book' must be a Book");
+    }
+    if (!library instanceof Library) {
+        throw Error("The element passed for 'library' must be a Library");
+    }
+    library.bookList.push(book);
 }
