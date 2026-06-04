390104eb9ee12111f6575ca13baa913b7e50930e
Thomas Lecoq <37746451+thomas-lecoq@users.noreply.github.com>
2026-06-04 14:41:51 +0200

    style(library): rename `maxSize` property as `size`

--- js/objects/library.js
+++ js/objects/library.js
@@ -1,21 +1,21 @@
 import { Book } from "./book.js"
 
-function Library(maxSize){
+function Library(size){
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
-    if (maxSize <= 0) {
+    if (size <= 0) {
         throw Error("A Library must have a non negative size");
     }
     this.collection = [];
-    this.maxSize = maxSize;
+    this.size = size;
 }
 
 function addBookToLibrary(book) {
     if (!(book instanceof Book)) {
         throw Error("The element passed for 'book' must be a Book");
     }
-    if (this.collection.length >= this.maxSize) {
+    if (this.collection.length >= this.size) {
         throw Error("The selected Library is full and cannot receive any new Book");
     }    
     this.collection.push(book);
