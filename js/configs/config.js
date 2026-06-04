6fb32fe3b059cbbb18b2661c6fe98e780d627235
Thomas Lecoq <37746451+thomas-lecoq@users.noreply.github.com>
2026-06-04 14:48:56 +0200

    refactor(init): move `books` const to config.js

--- js/configs/config.js
+++ js/configs/config.js
@@ -1,1 +1,7 @@
 // SCREAMING_CASE consts and all related configuration elements.
+
+export const BOOKS = [
+    ["The Hobbit", "J.R.R. Tolkien", 310, true],
+    ["Dune", "Frank Herbert", 412, false],
+    ["Sapiens", "Yuval Noah Harari", 443, true],
+];
