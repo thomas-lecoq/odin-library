f10d303e84fa50d7d43f40c6d2f87b2aa17954c3
Thomas Lecoq <37746451+thomas-lecoq@users.noreply.github.com>
2026-06-05 08:36:34 +0200

    feat(helpers) add `appendNewElement` function
    This utility function allows to add elements to the HTML and apply
    a CSS class to them. It avoids repeating the syntax for adding elements
    and improves code readability.

--- js/utils/helpers.js
+++ js/utils/helpers.js
@@ -1,1 +1,16 @@
-// Utilities functions
+// Utils functions
+
+/**
+ * Create an element with a class and append it to a parent node.
+ *
+ * @param {string} tagName - name of the HTML tag (ex: 'div')
+ * @param {string} className - CSS class to apply
+ * @param {HTMLElement} parent - parent element to append to
+ * @returns {HTMLElement} the newly created element
+*/
+export function appendNewElement(tagName, className, parent) {
+    const element = document.createElement(tagName);
+    element.classList.add(className);
+    parent.appendChild(element);
+    return element;
+}
