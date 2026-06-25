82dc36122079773564ec10a6390740ba6069bfe2
Thomas Lecoq <37746451+thomas-lecoq@users.noreply.github.com>
2026-06-25 20:51:30 +0200

    refactor(library): rewrite Library as an ES6 class

--- js/objects/library.js
+++ js/objects/library.js
@@ -1,43 +1,44 @@
 import { Book } from "./book.js"
 
-function Library(size){
-    if (!new.target) {
-        throw Error("You must use the 'new' operator to call the constructor");
-    }
-    if (size <= 0) {
-        throw Error("A Library must have a non negative size");
-    }
-    this.collection = new Map();
-    this.size = size;
-}
+export class Library {
+    #size;
+    #collection = new Map();
 
-function addBook(book) {
-    if (!(book instanceof Book)) {
-        throw Error("The element passed for 'book' must be a Book");
+    constructor(size) {
+        this.#size = this.#validateSize(size);
     }
-    if (this.collection.size >= this.size) {
-        throw Error("The selected Library is full and cannot receive any new Book");
-    }    
-    this.collection.set(book.id, book);
-}
-Library.prototype.addBook = addBook;
 
-function getBook(id) {
-    return this.collection.get(id);
-}
-Library.prototype.getBook = getBook;
-
-function removeBook(id) {
-    return this.collection.delete(id);
-}
-Library.prototype.removeBook = removeBook;
-
-function getLibraryData() {
-    const libraryData = {};
-    for (const [id, book] of this.collection) {
-        libraryData[id] = book.getBookData()
+    #validateSize(value) {
+        if (value <= 0) {
+            throw Error("A Library must have a positive size");
+        }
+        return value;
     }
-    return libraryData
+    get size() { return this.#size; }
+
+    addBook(book) {
+        if (!(book instanceof Book)) {
+            throw Error("The element passed for 'book' must be a Book");
+        }
+        if (this.#collection.size >= this.size) {
+            throw Error("The selected Library is full and cannot receive any new Book");
+        }
+        this.#collection.set(book.id, book);
+    }
+
+    getBook(id) {
+        this.#collection.get(id);
+    }
+
+    removeBook(id) {
+        this.#collection.delete(id);
+    }
+
+    getLibraryData() {
+        const libraryData = {};
+        for (const [id, book] of this.#collection) {
+            libraryData[id] = book.getBookData()
+        }
+        return libraryData;
+    }
 }
-Library.prototype.getLibraryData = getLibraryData;
-export { Library };
